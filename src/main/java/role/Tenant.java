876ed13577a5fbf3ade0844dd7a29b3ac22e2880
YIS <s12010059@stu.najah.edu>
2023-08-06 10:25:43 +0300

    update view available housings in tenant done.

--- src/main/java/role/Tenant.java
+++ src/main/java/role/Tenant.java
@@ -157,6 +157,7 @@ public class Tenant extends User{
             if (furniture.getName().equals(f)) {
                 fr = furniture;
             }
+
         }
         if(fr==null){
             System.out.println("Invalid Furniture!");
